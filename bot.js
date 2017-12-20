b462bd915fc046b781726e3842edc3353abbafee
Incien104 <33185021+Incien104@users.noreply.github.com>
2017-12-20 15:32:44 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -726,9 +726,7 @@ bot.on('message', message => {
 							// Send a private message
 							memberToAlert = message.guild.members.find('id', contributorID);
 							if (memberToAlert !== null) {									
-								memberToAlert.send({embed})
-								.catch(console.log("Message impossible to send to : "+contributorID));
-								//.catch(console.error);
+								memberToAlert.send({embed}).catch(console.error);
 							}
 							else {
 								botPostLog(contributorID+" est introuvable");
