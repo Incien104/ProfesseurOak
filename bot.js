315e4babd52eebb781aab5a382653c587ec8f19c
Incien104 <33185021+Incien104@users.noreply.github.com>
2017-12-26 21:26:31 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -148,7 +148,40 @@ bot.on('message', message => {
 				// Test function
 				case 'oaktest':
 					if (userRoles.find("name","@Admins")) {
+						var https = require('https');
+						https.get('https://professeur-oak.000webhostapp.com/functions/contributors.json', (res) => {
+						const { statusCode } = res;
+						const contentType = res.headers['content-type'];
 						
+						let error;
+						if (statusCode !== 200) {
+							error = new Error('Request Failed.\n' +
+											`Status Code: ${statusCode}`);
+						} else if (!/^application\/json/.test(contentType)) {
+							error = new Error('Invalid content-type.\n' +
+											`Expected application/json but received ${contentType}`);
+						}
+						if (error) {
+							console.error(error.message);
+							// consume response data to free up memory
+							res.resume();
+							return;
+						}
+						
+						res.setEncoding('utf8');
+						let rawData = '';
+						res.on('data', (chunk) => { rawData += chunk; });
+						res.on('end', () => {
+							try {
+							const parsedData = JSON.parse(rawData);
+							console.log(parsedData);
+							} catch (e) {
+							console.error(e.message);
+							}
+						});
+						}).on('error', (e) => {
+						console.error(`Got error: ${e.message}`);
+						});
 					} else {
 						message.reply("tu n'es pas autorisé à utiliser cette commande ! :no_entry: ");
 					}
