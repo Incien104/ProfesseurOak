f086c443a048c373f6265e04e09562e015fe9904
Incien104 <33185021+Incien104@users.noreply.github.com>
2018-01-07 11:52:14 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -808,7 +808,7 @@ bot.on('message', message => {
 							// Send a private message
 							memberToAlert = message.guild.members.find('id', contributorID);
 							if (memberToAlert !== null) {									
-								memberToAlert.send({embed}).catch(console.log('Un envoi n\'a pas abouti.'));
+								memberToAlert.send({embed}).catch("Envoi non abouti : "+contributorID);
 							} else {
 								botPostLog(contributorID+" est introuvable");
 							}
