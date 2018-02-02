e598248349e8e4be8ea9179c73cf1335ebb93225
Incien104 <jason.mejane@gadz.org>
2018-02-02 15:06:37 -0500

    v3.4.0 /2
    - fix syntax error in devCommand

--- modules/generalUtils/devCommand.js
+++ modules/generalUtils/devCommand.js
@@ -20,17 +20,15 @@ exports.test = (message) => {
 }
 
 // RESTART FUNCTION
-exports.restart = (message) => 
-	let userRoles = message.member.roles;
-	if (userRoles.find("name",rolesList.admin)) {
+exports.restart = (message) => {
+	if (message.member.roles.find("name",rolesList.admin)) {
 		generalFunc.replyDelete("commande désactivée ! Utiliser le bouton sur http://professeur-oak-sherbrooke.online",message);
 	}
 }
 
 // SCAN HUNTR INFO FUNCTION
 exports.scanHuntrConfig = (message) => {
-	let userRoles = message.member.roles;
-	if (userRoles.find("name",rolesList.admin)) {
+	if (message.member.roles.find("name",rolesList.admin)) {
 		let channelHuntr = message.guild.channels.find('name', chansLists.chanScanPokemon);
 		if !channelHuntr return;
 		channelHuntr.send("!setup 45.39652136952787,-71.88354492187501\n!radius 10\n!filter "+scanFilter.list);
@@ -41,8 +39,7 @@ exports.scanHuntrConfig = (message) => {
 
 // SCAN GYMHUNTR INFO FUNCTION
 exports.scanGymhuntrConfig = (message) => {
-	let userRoles = message.member.roles;
-	if (userRoles.find("name",rolesList.admin)) {
+	if (message.member.roles.find("name",rolesList.admin)) {
 		let channelHuntr = message.guild.channels.find('name', chansLists.chanScanPokemon);	
 		if !channelHuntr return;	
 		channelHuntr.send("!setup 45.39652136952787,-71.88354492187501\n!radius 10");
